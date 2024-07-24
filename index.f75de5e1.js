a3ee3e34979284a49b7e60c53d423eef08e84876
Roma Zuravliov <janmorozov1988@gmail.com>
2024-07-24 22:34:41 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,24 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const notification = document.createElement("div");
+    notification.className = `notification ${type}`;
+    notification.style.position = `absolute`;
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    const notificationTitle = document.createElement("h2");
+    notificationTitle.className = "title";
+    notificationTitle.textContent = title;
+    const notificationDescription = document.createElement("p");
+    notificationDescription.textContent = description;
+    notification.appendChild(notificationTitle);
+    notification.appendChild(notificationDescription);
+    document.body.appendChild(notification);
+    setTimeout(()=>{
+        document.body.removeChild(notification);
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
